5a706cfb821798eb63960125ad27fe5e8c4655df
GolibID <92590375+GolibID@users.noreply.github.com>
2023-12-11 21:03:48 +0900

    Update contact_script.js

--- js/contact_script.js
+++ js/contact_script.js
@@ -27,64 +27,8 @@ function sendEmail() {
   });
 }
 
-function checkInputs() {
-  const items = document.querySelectorAll(".item");
-
-  for (const item of items) {
-    if (item.value == "") {
-      item.classList.add("error");
-      item.parentElement.classList.add("error");
-    }
-
-    if (items[1].value != "") {
-      checkEmail();
-    }
-    items[1].addEventListener("keyup", () => {
-      checkEmail();
-    });
-
-    item.addEventListener("keyup", () => {
-      if (item.value == "") {
-        item.classList.remove("error");
-        item.parentElement.classList.remove("error");
-      } else {
-        item.classList.add("error");
-        item.parentElement.classList.add("error");
-      }
-    });
-  }
-}
-
-function checkEmail() {
-  const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
-  const errorTxtEmail = document.querySelector(".error-text email");
-  if (!email.value.match(emailRegex)) {
-    email.classList.add("error");
-    email.parentElement.classList.add("error");
-
-    if (email.value != "") {
-      errorTxtEmail.innerText = "Enter a valid email address";
-    } else {
-      errorTxtEmail.innerText = "Email Address can't be blank";
-    }
-  } else {
-    email.classList.remove("error");
-    email.parentElement.classList.remove("error");
-  }
-}
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  checkInputs();
 
-  if (
-    !fullName.classList.contains("error") &&
-    !email.classList.contains("error") &&
-    !phone.classList.contains("error") &&
-    !subject.classList.contains("error") &&
-    !mess.classList.contains("error")
-  ) {
-    sendEmail();
-  }
-
-  // sendEmail();
+  sendEmail();
 });
